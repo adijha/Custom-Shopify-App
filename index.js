3e382d40845b8895a0f1b56bb5f8f451e3dfd8d1
Aditya Kumar Jha <adityajha126@gmail.com>
2020-01-21 13:50:31 +0530

    heroku setup

--- index.js
+++ index.js
@@ -26,12 +26,12 @@ app.get('/', (req, res) => {
 
 const port = process.env.PORT || 8000;
 
-// if (process.env.NODE_ENV === 'production') {
-app.use(express.static('client/build'));
-app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
-// }
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
