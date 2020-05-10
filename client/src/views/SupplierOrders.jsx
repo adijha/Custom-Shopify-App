51215ea2f6617e6dbdc63b5767c929fede046f23
adijha <mail@adijha.com>
2020-05-11 02:09:40 +0530

    expanded with react

--- client/src/views/SupplierOrders.jsx
+++ client/src/views/SupplierOrders.jsx
@@ -1,228 +1,144 @@
-import React, { Component, useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import "../assets/css/supplierOrders.css";
-import Card from "../components/Card/Card.jsx";
-// import { Grid, Row, Col, Table } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Grid, Row, Col, Table } from "react-bootstrap";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
 import Modal from "react-responsive-modal";
 
-const token = localStorage.getItem("token");
-const decode = jwt_decode(token);
-var store = {
-  headerOffset: null,
-};
+import Card from "../components/Card/Card.jsx";
 
-let data = [
-  {
-    id: 0,
-    name: "name 0",
-    details: "details 0",
-    state: "live",
-  },
-  {
-    id: 1,
-    name: "name 1",
-    details: "details 1",
-    state: "live",
-  },
-  {
-    id: 2,
-    name: "name 2",
-    details: "details 2",
-    state: "draft",
-  },
-];
+const SupplierOrders = () => {
+  const token = localStorage.getItem("token");
+  const decode = jwt_decode(token);
+  const [orderList, setOrderList] = useState([
+    {
+      customer: {
+        address: "28 A, N-3, Mojitolabs, DLF phase 2, Gurugramnull",
+        city: "Gurugram",
+        email: "devadityajha@gmail.com",
+        name: "AdityaJha",
+        phone: 7821915962,
+        zip: 122010,
+      },
+      id: "2148014227535",
+      name: "New Product variants",
+      quantity: 1,
+      sku: "SKU1001",
+    },
+    {
+      customer: {
+        address: "28 A, N-3, Mojitolabs, DLF phase 2, Gurugramnull",
+        city: "Gurugram",
+        email: "devadityajha@gmail.com",
+        name: "AdityaJha",
+        phone: 7821915962,
+        zip: 122010,
+      },
+      id: "2148014227535",
+      name: "New Product variants",
+      quantity: 1,
+      sku: "SKU1001",
+    },
+    {
+      customer: {
+        address: "28 A, N-3, Mojitolabs, DLF phase 2, Gurugramnull",
+        city: "Gurugram",
+        email: "devadityajha@gmail.com",
+        name: "AdityaJha",
+        phone: 7821915962,
+        zip: 122010,
+      },
+      id: "2148014227535",
+      name: "New Product variants",
+      quantity: 1,
+      sku: "SKU1001",
+    },
+  ]);
 
-let cols = [
-  {
-    icon: "",
-    label: "Order ID",
-  },
-  {
-    icon: "",
-    label: "SKU",
-  },
-  {
-    icon: "",
-    label: "Customer",
-  },
-  {
-    icon: "",
-    label: "State",
-  },
-];
+  useEffect(() => {
+    // getOrderList();
+  }, []);
 
-class RowItem extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      open: false,
-    };
-  }
-
-  toggleRow(e) {
-    console.log("toggleRow");
-
-    this.setState({ open: !this.state.open });
-  }
-
-  render() {
-    let classes = "";
-    if (this.state.open) {
-      classes = "open";
-    }
-
-    return (
-      <li onClick={this.toggleRow.bind(this)} className={classes}>
-        <div className="heading">
-          <div className="col">{this.props.id}</div>
-          <div className="col">{this.props.name}</div>
-          <div className="col">{this.props.details}</div>{" "}
-          <div className="col">{this.props.state}</div>
-        </div>
-        <RowContent open={this.state.open} />
-        {this.props.children}
-      </li>
-    );
-  }
-}
-
-class RowContent extends React.Component {
-  clicker() {}
-
-  render() {
-    let jsxhtml = (
-      <div className="content" onClick={this.clicker.bind(this)}>
-        row content
-        {this.props.children}
-      </div>
-    );
-
-    if (this.props.open) {
-      jsxhtml = (
-        <div className="content open" onClick={this.clicker.bind(this)}>
-          row content
-          {this.props.children}
-        </div>
-      );
-    }
-
-    return <div>{jsxhtml}</div>;
-  }
-}
-
-class Table extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      headerOffset: null,
-      headerFixed: true,
-    };
-  }
-
-  //   handleScroll(e) {
-  //     let scrollTop = e.srcElement.body.scrollTop;
-  //     console.log('scroll...', scrollTop, this.state.headerOffset);
-  //     this.setState({
-  //       headerFixed: true
-  //     });
-  //   }
-
-  componentDidMount() {
-    //   window.addEventListener('scroll', this.handleScroll.bind(this));
-    // THIS SEEMS THE ONLY PLACE WE CAN PICK UP THE REF FOR THE HEADER
-    console.log("reactdom: ", ReactDOM.findDOMNode(this.refs.header));
-    store.headerOffset = ReactDOM.findDOMNode(
-      this.refs.header
-    ).getBoundingClientRect().top;
-    console.log("store:", store.headerOffset);
-    // this.setState({headerOffset:ReactDOM.findDOMNode(this.refs.header)});
-  }
-
-  render() {
-    let columns = this.props.columns.map((item, inx) => {
-      return <HeaderColumn label={item.label} />;
-    });
-    //go through the rows
-    let rows = this.props.data.map((item, inx) => {
-      return <RowItem {...item}></RowItem>;
-    });
-    let classes = "header";
-    if (this.props.headerFixed) {
-      classes = "header fixed";
-    }
-    return (
-      <div className="table">
-        {this.props.children}
-        <div className={classes} ref="header">
-          {columns}
-          <div className="shadow"></div>
-        </div>
-        <ul>{rows}</ul>
-      </div>
-    );
-  }
-}
-
-class HeaderColumn extends React.Component {
-  constructor() {
-    super();
-  }
-  render() {
-    return <div className="hcol">{this.props.label}</div>;
-  }
-}
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      tableHeader: null,
-      tableHeaderFixed: false,
-      ordersData: "",
-    };
-  }
-
-  handleScroll(e) {
-    let scrollTop = e.srcElement.body.scrollTop;
-    console.log("app scroll...", scrollTop, store.headerOffset);
-    if (scrollTop >= store.headerOffset) {
-      this.setState({
-        tableHeaderFixed: true,
-      });
-    } else {
-      this.setState({
-        tableHeaderFixed: false,
-      });
-    }
-  }
-  getOrderList = () => {
-    axios.get("/api/ordersList/" + decode.id).then((res) => {
-      this.setState({ ordersData: res.data });
+  const getOrderList = () => {
+    axios.get("/api/ordersList/" + decode.id).then((data) => {
+      setOrderList(data.data);
+      console.log(data.data);
     });
   };
 
-  componentDidMount() {
-    this.getOrderList();
-    window.addEventListener("scroll", this.handleScroll.bind(this));
-  }
+  return (
+    <div className="content">
+      <Grid fluid>
+        <Row>
+          <Col md={12}>
+            <Card
+              title="Order List"
+              category={"Total Orders :" + orderList.length}
+              ctTableFullWidth
+              ctTableResponsive
+              content={
+                <Table striped hover size="sm">
+                  <thead>
+                    <tr>
+                      <th>Id</th>
+                      <th>SKU</th>
+                      <th>Name</th>
+                      <th>Quantity</th>
+                      <th>DElivery Add.</th>
+                      <th>Track No.</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {orderList.map((item, key) => {
+                      return (
+                        <>
+                          <tr
+                            key={key}
+                            onClick={() => {
+                              console.log("item", item.name);
+                            }}
+                          >
+                            <td>{item.id}</td>
+                            <td>{item.sku}</td>
+                            <td>{item.name}</td>
+                            <td>{item.quantity}</td>
 
-  render() {
-    return (
-      <div className="content">
-        <div className="order-container">
-          <Table
-            data={data}
-            columns={cols}
-            headerFixed={this.state.tableHeaderFixed}
-            scrollFn=""
-          />
-        </div>
-      </div>
-    );
-  }
-}
+                            <td>
+                              {item.customer.address},{item.customer.city}-
+                              {item.customer.zip}, Mob. no.-
+                              {item.customer.phone}
+                            </td>
 
-export default App;
+                            <td>
+                              {" "}
+                              <input type="text" name="track_details" />
+                            </td>
+                            <td>
+                              <button className="btn btn-primary btn-sm">
+                                Deliver
+                              </button>
+                            </td>
+                          </tr>
+                          <tr
+                            key={key}
+                            onClick={() => {
+                              console.log("item", item.name);
+                            }}
+                          >
+                            <td>Customer Details</td>
+                            <td>Order Details</td>
+                          </tr>
+                        </>
+                      );
+                    })}
+                  </tbody>
+                </Table>
+              }
+            />
+          </Col>
+        </Row>
+      </Grid>
+    </div>
+  );
+};
+
+export default SupplierOrders;
