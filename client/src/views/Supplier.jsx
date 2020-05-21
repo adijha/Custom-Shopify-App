e2f7ad199c88fbced52a621ae174b3ec8cfb5572
Ashish Goyal <goyal.ashish062@gmail.com>
2020-05-21 11:07:30 +0530

    add supplier password hide/show

--- client/src/views/Supplier.jsx
+++ client/src/views/Supplier.jsx
@@ -7,6 +7,8 @@ const [name, setName] = useState("");
 const [email, setEmail] = useState("");
 const [password, setPassword] = useState("");
 const [status, setStatus] = useState("");
+const [typeCheck, setTypeCheck] = useState(true)
+const [type, setType]= useState("text")
 
 const updateName = e =>{
   setName(e.target.value)
@@ -19,6 +21,22 @@ const updateEmail = e =>{
 const updatePwd = e=>{
   setPassword(e.target.value)
 }
+
+
+const handleClick = ()=>{
+  setTypeCheck(!typeCheck)
+  if (typeCheck==true) {
+    setType("text")
+  }
+  else{
+    setType("password")
+  }
+}
+
+// const  handleClick = () => setType(({type})=>({
+//   type: type==="text"? "password": "text"
+// }))
+
 
 const addSuplier = e =>{
   e.preventDefault();
@@ -58,7 +76,12 @@ const addSuplier = e =>{
            </div>
            <div className='password'>
              <label htmlFor="password">Password</label>
-             <input type='password' name='password'  value={password} onChange={updatePwd} />
+             <input type={type} name='password'  value={password} onChange={updatePwd} />
+             <span className="password__show text-right" onClick={handleClick}>  <svg className="bi bi-eye" width="2em" height="1.5em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+         <path fillRule="evenodd" d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.134 13.134 0 001.66 2.043C4.12 11.332 5.88 12.5 8 12.5c2.12 0 3.879-1.168 5.168-2.457A13.134 13.134 0 0014.828 8a13.133 13.133 0 00-1.66-2.043C11.879 4.668 10.119 3.5 8 3.5c-2.12 0-3.879 1.168-5.168 2.457A13.133 13.133 0 001.172 8z" clipRule="evenodd" />
+         <path fillRule="evenodd" d="M8 5.5a2.5 2.5 0 100 5 2.5 2.5 0 000-5zM4.5 8a3.5 3.5 0 117 0 3.5 3.5 0 01-7 0z" clipRule="evenodd" />
+       </svg>
+ </span>
            </div>
            <div className='info'>
              <small>Password must be eight characters in length.</small>
