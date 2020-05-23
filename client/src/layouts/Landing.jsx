5a7f3ceba39fd68c81deb7657a5f9e0ff54d086a
Ashish Goyal <goyal.ashish062@gmail.com>
2020-05-23 19:04:36 +0530

    landing page changes done

--- client/src/layouts/Landing.jsx
+++ client/src/layouts/Landing.jsx
@@ -374,34 +374,44 @@ position: "relative"}}/></Navbar.Brand>
        <div className="footer-top-layout">
          <div className="container">
            <div className="row">
-             <div className=" col-lg-8 col-lg-offset-2">
-               <div className="col-sm-4" style={{float:"left"}}>
-                 <div className="footer-col-item" style={{float:"left"}}>
-                 <img className="d-inline-block align-left"
+             <div className="col-md-12" style={{height:"100px"}}>
+               <div className="col-sm-3">
+                 <img className="footer-img"
               src={require('../assets/img/latestLogo.png')}/>
-                 </div>
                </div>
-               <div className="col-sm-4">
-                 <div className="footer-col-item">
-                   <h4>Reach Us</h4>
-                   <div className="item-contact"> <a href="tel:630-885-9200"><span className="link-id">P</span>:<span>630-885-9200</span></a> <a href="tel:630-839.2006"><span className="link-id">F</span>:<span>630-839.2006</span></a> <a href="mailto:info@brandcatmedia.com"><span className="link-id">E</span>:<span>info@brandcatmedia.com</span></a> </div>
-                 </div>
+               <div className="col-sm-3">
+                    <a className="btn btn-primary" style={{backgroundColor:"white", float:"right"}} href="#">Become An Affiliate</a>
                </div>
-               <div className="col-sm-4">
-                 <div className="footer-col-item">
-                   <h4>Sign up for Newsletter</h4>
-                   <form className="signUpNewsletter" action method="get">
-                     <input name className="gt-email form-control" placeholder="You@youremail.com" type="text" />
-                     <input name className="btn-go" defaultValue="Go" type="button" />
-                   </form>
-                 </div>
+               <div className="col-sm-6">
+               <div className="text-center footer-social">
+               <a href="#"> Support</a>
+               <a href="/login-merchant"> Login</a>
+               <a href="/merchant-signup">Sign Up</a>
+
+               </div>
+
                </div>
              </div>
            </div>
          </div>
        </div>
        <div className="footer-bottom-layout">
-         <div className="copyright-tag">Copyright © 2017 company name. All Rights Reserved.</div>
+       <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+          <p>Copyright © 2020 MelisXpress.com</p>
+          </div>
+          <div className="col-md-8 footer-company">
+
+            <a className="btn btn-info" href="#">Support</a>
+            <a href="#">Terms & Conditions</a>
+            <a href="#">Privacy Policy</a>
+            <a href="#">Become a Supplier</a>
+
+
+          </div>
+        </div>
+       </div>
        </div>
      </footer>
      </section>
