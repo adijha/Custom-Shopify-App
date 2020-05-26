b2ffa7f6f8ebe23a14b569a6b57936c25e4cd17a
adijha <mail@adijha.com>
2020-05-26 22:15:15 +0530

    done

--- client/src/views/SupplierForm.jsx
+++ client/src/views/SupplierForm.jsx
@@ -28,23 +28,35 @@ const SupplierForm = () => {
       <div className='container-fluid' style={{ maxWidth: 800 }}>
         <div
           className='sup-form-tabs'
-          style={{ display: 'flex', justifyContent: 'space-between' }}
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginBottom: 30,
+          }}
         >
           <div
             className='sup-form-tab'
             style={{ cursor: 'pointer' }}
             onClick={() => setTab(1)}
           >
-            <h5 className='sup-form-tab-inner'>Personal Details</h5>
+            <h5
+              className='sup-form-tab-inner'
+            >
+              Personal Details
+            </h5>
+            {tab === 1 ? <hr className='tab-hr' /> : null}
           </div>
           <div onClick={() => setTab(2)} style={{ cursor: 'pointer' }}>
             <h5>Company Details</h5>
+            {tab === 2 ? <hr className='tab-hr' /> : null}
           </div>
           <div onClick={() => setTab(3)} style={{ cursor: 'pointer' }}>
             <h5>Shipping Details</h5>
+            {tab === 3 ? <hr className='tab-hr' /> : null}
           </div>
           <div onClick={() => setTab(4)} style={{ cursor: 'pointer' }}>
             <h5>Branding and Return Management</h5>
+            {tab === 4 ? <hr className='tab-hr' /> : null}
           </div>
         </div>
         <div>
@@ -52,7 +64,7 @@ const SupplierForm = () => {
             <div>
               <div className='card card-input'>
                 <div className='form-group'>
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     Full Name
                   </h5>
                   <input
@@ -63,7 +75,7 @@ const SupplierForm = () => {
                     onChange={(e) => setName(e.target.value)}
                     required
                   />
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     Email Address
                   </h5>
                   <input
@@ -74,7 +86,7 @@ const SupplierForm = () => {
                     onChange={(e) => setEmail(e.target.value)}
                     required
                   />
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     Phone Number with country code
                   </h5>
                   <input
@@ -100,7 +112,7 @@ const SupplierForm = () => {
             <div>
               <div className='card card-input'>
                 <div className='form-group'>
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     Business Name
                   </h5>
                   <input
@@ -111,7 +123,7 @@ const SupplierForm = () => {
                     onChange={(e) => setBusinessName(e.target.value)}
                     required
                   />
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     Business Website
                   </h5>
                   <input
@@ -122,7 +134,7 @@ const SupplierForm = () => {
                     onChange={(e) => setWebsite(e.target.value)}
                     required
                   />
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     VAT or Tax ID
                   </h5>
                   <input
@@ -133,7 +145,7 @@ const SupplierForm = () => {
                     onChange={(e) => setVAT(e.target.value)}
                     required
                   />
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     What and all categories of product you are dealing with?
                   </h5>
                   <input
@@ -144,7 +156,7 @@ const SupplierForm = () => {
                     onChange={(e) => setCategories(e.target.value)}
                     required
                   />
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     Please tell us your different warehouse locations around the
                     world
                   </h5>
@@ -175,7 +187,7 @@ const SupplierForm = () => {
             <div>
               <div className='card card-input'>
                 <div className='form-group'>
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     Do you use e-packet shipping? (Yes/No)
                   </h5>
                   <input
@@ -186,7 +198,7 @@ const SupplierForm = () => {
                     onChange={(e) => setEpacket(e.target.value)}
                     required
                   />
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     If No, then What shipping method you use for e-packet
                     countries?
                   </h5>
@@ -196,9 +208,9 @@ const SupplierForm = () => {
                     placeholder='Enter shipping method'
                     value={epacketShipping}
                     onChange={(e) => setEpacketShipping(e.target.value)}
-                    required
+                 
                   />
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     Please tell us the approximate processing time of the order.
                     (We are only looking for suppliers who can agree to process
                     the order in 1-3 days)
@@ -211,7 +223,7 @@ const SupplierForm = () => {
                     onChange={(e) => setProcessing(e.target.value)}
                     required
                   />
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     Approximate number of days for delivery to e-packet
                     countries- Non e-packet countries-
                   </h5>
@@ -223,7 +235,7 @@ const SupplierForm = () => {
                     onChange={(e) => setNonEpacket(e.target.value)}
                     required
                   />
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     Do you provide fastest shipping or express shipping to US?
                     Please tell us the shipping courier name, shipping charges
                     and approximate days of delivery.
@@ -240,7 +252,6 @@ const SupplierForm = () => {
                     disabled={
                       epacket &&
                       processing &&
-                      epacketShipping &&
                       nonEpacket &&
                       fastUS
                         ? false
@@ -259,7 +270,7 @@ const SupplierForm = () => {
             <div>
               <div className='card card-input'>
                 <div className='form-group'>
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     Will you be able to provide branded invoicing? (Yes/No)
                   </h5>
                   <input
@@ -270,7 +281,7 @@ const SupplierForm = () => {
                     onChange={(e) => setBrandedInvoice(e.target.value)}
                     required
                   />
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     Will you be able to branded packaging? (Yes/No)
                   </h5>
                   <input
@@ -281,7 +292,7 @@ const SupplierForm = () => {
                     onChange={(e) => setBrandedPackaging(e.target.value)}
                     required
                   />
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     What is the minimum number of orders you require for branded
                     packaging?
                   </h5>
@@ -293,7 +304,7 @@ const SupplierForm = () => {
                     onChange={(e) => setMinimumOrder(e.target.value)}
                     required
                   />
-                  <h5 style={{ marginBottom: -7, marginTop: 13, fontSize: 15 }}>
+                  <h5 style={{ marginBottom: -7, marginTop: 15, fontSize: 15 }}>
                     If we have any return in case of damage or any other reason.
                     Will you buy back or replace it and let us know your return
                     management.
@@ -306,6 +317,17 @@ const SupplierForm = () => {
                     onChange={(e) => setReturnManagement(e.target.value)}
                     required
                   />
+                  <p className='text-center'>
+                    <span className='check'>
+                      <input
+                        type='checkbox'
+                        required
+                        value={terms}
+                        onChange={() => setTerms(true)}
+                      />
+                    </span>
+                    I agree to the terms & conditions
+                  </p>
                   <button
                     disabled={
                       returnManagement &&
@@ -319,7 +341,7 @@ const SupplierForm = () => {
                     className='signup-btn'
                     onClick={() => setTab(2)}
                   >
-                    Done
+                    Submit
                   </button>
                 </div>
               </div>
