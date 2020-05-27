b92823c456996c73b487489085a460ed28e0407b
Ashish Goyal <goyal.ashish062@gmail.com>
2020-05-27 17:38:03 +0530

    header fix in supplier form

--- client/src/views/SupplierForm.jsx
+++ client/src/views/SupplierForm.jsx
@@ -3,6 +3,7 @@ import '../assets/css/SupplierForm.css';
 import PhoneInput from 'react-phone-number-input';
 import axios from 'axios'
 import { NotificationManager } from 'react-notifications';
+import { Navbar, Nav, NavDropdown, Form,  FormControl, Button, Table} from 'react-bootstrap';
 
 const SupplierForm = () => {
   const [tab, setTab] = useState(1);
@@ -43,7 +44,45 @@ NotificationManager.error('Something wrong');
 
   return (
     <div>
-      <div style={{ height: '100px' }}></div>
+    <div style={{height:"100px"}}>
+    <Navbar bg="light" expand="lg" style={{border:"none"}}>
+    <div className="container">
+    <div className="row">
+    <div className="col-md-6 head-img">
+    <Navbar.Brand href="#"><img        className="d-inline-block align-top navBrand"
+ src={require('../assets/img/latestLogo.png')} style={{float:"left",display: "flex",
+height: "300px",
+padding: "0",
+top: "-12rem",
+position: "relative"}}/></Navbar.Brand>
+</div>
+<div className="col-md-6" style={{}}>
+
+<div className="collapse navbar-collapse" id="navbarSupportedContent" style={{color:"blue"}}>
+
+         <Nav className="mr-auto login" style={{ color:"blue", float:"right"}}>
+
+           <NavDropdown title="Login" id="basic-nav-dropdown" >
+               <li><a href="/login-admin" className="text-center" style={{float:"left", color:"black"}}>Admin <span className="glyphicon glyphicon-cog pull-left" /></a></li>
+               <li><a href="/login-supplier" style={{float:"left", color:"black"}}>Supplier <span className="glyphicon glyphicon-stats pull-left" /></a></li>
+               <li><a href="/login-merchant" style={{float:"left", color:"black"}}>Merchant <span className="glyphicon glyphicon-stats pull-left" /></a></li>
+           </NavDropdown>
+         </Nav>
+
+     </div>
+
+
+
+
+</div>
+</div>
+</div>
+  </Navbar>
+     </div>
+
+     {/* Form*/}
+     <section id="banner">
+      <div className="formTab" style={{ height: '100px' }}></div>
       <div className='container-fluid' style={{ maxWidth: 800 }}>
         <div
           className='sup-form-tabs'
@@ -384,6 +423,7 @@ NotificationManager.error('Something wrong');
           ) : null}
         </div>
       </div>
+      </section>
       <section id='footer'>
         <footer id='footer-Section'>
           <div className='footer-top-layout'>
