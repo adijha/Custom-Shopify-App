8f38574e38de1fc6b97b754e70ecb64a102068ea
adijha <mail@adijha.com>
2020-06-03 02:46:48 +0530

    merchant detail csv download

--- client/src/views/MerchantDetail.jsx
+++ client/src/views/MerchantDetail.jsx
@@ -1,40 +1,59 @@
 import React, { useState, useEffect } from 'react';
 import { Grid, Row, Col, Table } from 'react-bootstrap';
+import { CSVLink, CSVDownload } from 'react-csv';
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 import { NotificationManager } from 'react-notifications';
 import Card from '../components/Card/Card.jsx';
 import CustomButton from '../components/CustomButton/CustomButton';
 import '../assets/css/supplierOrders.css';
-import MerchantAccountDetail from './MerchantAccountDetail.jsx'
-
+import MerchantAccountDetail from './MerchantAccountDetail.jsx';
 
 const MerchantDetail = () => {
-
-
   const [expand, setExpand] = useState('');
   const [merchantList, setMerchantList] = useState([]);
   const [fulfill, setFulfill] = useState('');
-  const [orderDetail, setOrderDetail]=useState("")
-
+  const [orderDetail, setOrderDetail] = useState('');
 
   useEffect(() => {
     getMerchant();
   }, []);
 
-
   const getMerchant = async () => {
-
-      const res = await axios.get('/api/customMerchantDetail')
-      setMerchantList(res.data)
-      console.log(res.data)
+    const res = await axios.get('/api/customMerchantDetail');
+    setMerchantList(res.data);
+    console.log(res.data);
   };
-
-
-
 
   return (
     <div className='content'>
+      <div
+        style={{
+          textAlign: 'right',
+          alignSelf: 'right',
+          display: 'flex',
+          justifyContent: 'flex-end',
+          marginTop: '-20px',
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: 'grey',
+            width: '120px',
+            height: '40px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 10,
+            marginRight: '18px',
+            marginBottom: '10px',
+          }}
+        >
+          <CSVLink data={merchantList} style={{ color: '#fff' }}>
+            Download CSV
+          </CSVLink>
+        </div>
+      </div>
       <Grid fluid>
         <Row>
           <Col md={12}>
@@ -60,25 +79,29 @@ const MerchantDetail = () => {
                       return (
                         <>
                           <tr
-                          key={key}
-                          onClick={() => {
-                            if (expand === item.id) {
-                              setExpand(null);
-                            } else {
-                              setExpand(item.id);
-                            }
-                          }}
+                            key={key}
+                            onClick={() => {
+                              if (expand === item.id) {
+                                setExpand(null);
+                              } else {
+                                setExpand(item.id);
+                              }
+                            }}
                           >
-                            <td>{item.firstName || 'NA'} {item.lastName || ''}</td>
+                            <td>
+                              {item.firstName || 'NA'} {item.lastName || ''}
+                            </td>
                             <td>{item.email || 'NA'}</td>
                             <td>{item.joiningDate || 'NA'}</td>
                             <td>{item.plan || 'NA'}</td>
                             <td>{item.count || 'NA'}</td>
                             <td>&#x24; {item.price || 'NA'}</td>
-                            <td><a href={'/admin/merchant/'+item.id}>View More</a></td>
+                            <td>
+                              <a href={'/admin/merchant/' + item.id}>
+                                View More
+                              </a>
+                            </td>
                           </tr>
-
-                        
                         </>
                       );
                     })}
