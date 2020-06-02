385540b9193242f6bcd5cb30f272cef475a84db6
adijha <mail@adijha.com>
2020-06-02 23:01:43 +0530

    lint

--- utils/resuableFunction.js
+++ utils/resuableFunction.js
@@ -1,14 +1,9 @@
 const Orders = require("../model/Orders");
 const Products = require("../model/Products");
-
-
-
 const revenueSupplier = async (id) =>{
-  console.log("id is in resuable", id);
+  // console.log("id is in resuable", id);
   let itemArray = [];
-
   const data = await Orders.find({});
-
   data.forEach((item, i) => {
     item.products.forEach((sss, i) => {
       if (sss.sku !== undefined) {
@@ -19,9 +14,7 @@ const revenueSupplier = async (id) =>{
       }
     });
   });
-
   var holder = {};
-
   itemArray.forEach(function (d) {
     if (holder.hasOwnProperty(d.sku)) {
       holder[d.sku] = holder[d.sku] + d.count;
@@ -31,28 +24,23 @@ const revenueSupplier = async (id) =>{
   });
 
   var obj2 = [];
-
   for (var prop in holder) {
     obj2.push({ sku: prop, count: holder[prop] });
   }
-
   let calPrice = [];
   const productData = await Products.find({ supplier_id: id });
-
-  console.log(productData.length);
-
+  // console.log(productData.length);
   obj2.forEach((arr, i) => {
     productData.forEach((product, j) => {
       if (product.code === arr.sku) {
         let countPrice = product.price * arr.count;
-
         calPrice.push(~~countPrice);
       }
     });
   });
-  console.log({ calPrice });
+  // console.log({ calPrice });
   let income = calPrice.reduce((a, b) => a + b, 0);
-  console.log(income);
+  // console.log(income);
   return income;
   // res.status(200).json(income);
 }
@@ -91,13 +79,13 @@ const orderSupplier = async (id) =>{
   for (var prop in holder) {
     obj2.push({ sku: prop, count: holder[prop] });
   }
-  //console.log({obj2});
+  // console.log({obj2});
 
   let calOrder = [];
 
   const productData = await Products.find({ supplier_id: id });
 
-  //console.log(productData.length);
+  // console.log(productData.length);
 
   obj2.forEach((arr, i) => {
     productData.forEach((product, j) => {
@@ -108,9 +96,9 @@ const orderSupplier = async (id) =>{
       }
     });
   });
-  console.log({ calOrder });
+  // console.log({ calOrder });
   let totalOrders = calOrder.reduce((a, b) => a + b, 0);
-  console.log(totalOrders);
+  // console.log(totalOrders);
   //res.status(200).json(totalOrders);
   return totalOrders;
 }
