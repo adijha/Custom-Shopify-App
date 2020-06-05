a31e41e40551489f00c23ce4c5be67870a12c06d
adijha <mail@adijha.com>
2020-06-05 18:04:58 +0530

    data shown

---
+++ client/src/views/AdminOrderDetails.jsx
@@ -0,0 +1,188 @@
+import React, { useState, useEffect } from 'react';
+import { Grid, Row, Col, Table } from 'react-bootstrap';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import Modal from 'react-responsive-modal';
+import Card from '../components/Card/Card.jsx';
+import CsvDownloader from 'react-csv-downloader';
+
+const SupplierList = () => {
+  const [suppliers, setSuppliers] = useState([]);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [status, setStatus] = useState('');
+  const [tempSupplier, setTempSupplier] = useState('');
+  const [open, setOpen] = useState(false);
+
+  const [sRevenue, setSRevenue] = useState();
+  const [sOrder, setSOrder] = useState();
+  const [productCount, setProductCount] = useState();
+
+  useEffect(() => {
+    getSupplierData();
+  }, []);
+
+  const getSupplierData = async () => {
+    axios.get('/api/customOrderDetails').then((res) => {
+      setSuppliers(res.data);
+      console.log(res.data);
+    });
+  };
+
+  const updateSupplier = (item) => {
+    console.log(item._id);
+    setOpen(true);
+    setEmail(item.email);
+    setName(item.supplier_id);
+    setTempSupplier(item._id);
+  };
+
+  const submitUpdateSuplier = (e) => {
+    e.preventDefault();
+    const obj = {
+      _id: tempSupplier,
+      supplier_id: name,
+      email: email,
+      password: password,
+    };
+    console.log(obj);
+    axios.patch('/api/update', obj).then((data) => {
+      if (data) {
+        setStatus('Supplier Updated Successfully');
+        setOpen(false);
+      }
+    });
+  };
+
+  const handleClickMe = () => {};
+
+  const updatebtn = {
+    width: '50%',
+  };
+  return (
+    <div className='content'>
+      <div
+        style={{
+          // backgroundColor: 'green',
+          textAlign: 'right',
+          alignSelf: 'right',
+          display: 'flex',
+          justifyContent: 'flex-end',
+          marginTop: '-20px',
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: 'grey',
+            width: '120px',
+            height: '40px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            borderRadius: 10,
+            marginRight: '18px',
+            marginBottom: '10px',
+          }}
+        >
+          <CsvDownloader
+            filename='AdminOrderDetails'
+            separator=','
+            wrapColumnChar="'"
+            datas={suppliers}
+          >
+            <h5 style={{ marginTop: 15 }}>Download</h5>
+          </CsvDownloader>
+        </div>
+      </div>
+      <div className='info text-center' style={{ color: 'red' }}>
+        {status}
+      </div>
+
+      <Grid fluid>
+        <Row>
+          <Col md={12}>
+            <Card
+              ctTableFullWidth
+              ctTableResponsive
+              content={
+                <Table striped hover size='sm'>
+                  <thead>
+                    <tr>
+                      <th>Product Image</th>
+                      <th>Order Id</th>
+                      <th>Merchant Id</th>
+                      <th>Supplier Id</th>
+                      <th>Customer</th>
+                      <th>SKU</th>
+                      <th>Total</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {suppliers.map((item, key) => {
+                      return (
+                        <tr key={key}>
+                          <td>{key + 1}</td>
+                          <td>{item.orderId}</td>
+                          <td>{item.merchantName}</td>
+                          <td>{item.supplier_id}</td>
+                          <td>{item.customer_name}</td>
+                          <td>{item.sku}</td>
+                          <td>{item.total_price}</td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </Table>
+              }
+            />
+          </Col>
+        </Row>
+      </Grid>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <br />
+        <h3 style={{ color: 'blue' }} className='text-center'>
+          Edit Supplier Details:
+        </h3>
+        <form onSubmit={submitUpdateSuplier}>
+          <div className='fullName'>
+            <label htmlFor='fullName'>Supplier Id</label>
+            <input
+              type='text'
+              name='fullName'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div className='email'>
+            <label htmlFor='email'>Email</label>
+            <input
+              type='email'
+              name='email'
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+          <div className='password'>
+            <label htmlFor='password'>New Password</label>
+            <input
+              type='password'
+              name='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div style={{ margin: 'auto' }}>
+            <button type='submit' className='btn btn-primary' style={updatebtn}>
+              Update
+            </button>
+          </div>
+        </form>
+      </Modal>
+    </div>
+  );
+};
+
+export default SupplierList;
