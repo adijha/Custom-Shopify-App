e25fedea8c6e52602607073a3630ca6f59a12259
adijha <mail@adijha.com>
2020-06-05 17:29:45 +0530

    pack

--- client/src/views/SupplierList.jsx
+++ client/src/views/SupplierList.jsx
@@ -1,28 +1,11 @@
-/*!
-
-=========================================================
-* Light Bootstrap Dashboard React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React, {useState,useEffect} from "react";
 import { Grid, Row, Col, Table } from "react-bootstrap";
 import axios from 'axios';
 import jwt_decode from 'jwt-decode';
 import Modal from "react-responsive-modal";
-
-
 import Card from "../components/Card/Card.jsx";
+import CsvDownloader from 'react-csv-downloader';
+
 
 const SupplierList = () => {
 
