7917a80bb2120d209eee96c9556637dbe203010a
adijha <mail@adijha.com>
2020-07-10 09:49:50 +0530

    dd

--- client/src/views/CopyDashboard.jsx
+++ client/src/views/CopyDashboard.jsx
@@ -1,10 +1,10 @@
-import React, { Component, useEffect, useState } from "react";
-import ChartistGraph from "react-chartist";
-import { Grid, Row, Col, Table } from "react-bootstrap";
+import React, { Component, useEffect, useState } from 'react';
+import ChartistGraph from 'react-chartist';
+import { Grid, Row, Col, Table } from 'react-bootstrap';
 
-import { Card } from "../components/Card/Card.jsx";
-import { StatsCard } from "../components/StatsCard/StatsCard.jsx";
-import { Tasks } from "../components/Tasks/Tasks.jsx";
+import { Card } from '../components/Card/Card.jsx';
+import { StatsCard } from '../components/StatsCard/StatsCard.jsx';
+import { Tasks } from '../components/Tasks/Tasks.jsx';
 import {
   dataPie,
   legendPie,
@@ -15,16 +15,16 @@ import {
   optionsBar,
   responsiveBar,
   legendBar,
-} from "../variables/Variables.jsx";
+} from '../variables/Variables.jsx';
 
-import axios from "axios";
+import axios from 'axios';
 
 const CopyDashboard = () => {
-  const [count, setCount] = useState("");
-  const [revenue, setRevenue] = useState("");
+  const [count, setCount] = useState('');
+  const [revenue, setRevenue] = useState('');
   const [graphPlot, setGraphPlot] = useState({});
   const [piePlot, setPiePlot] = useState({});
-  const [mCount, setMCount] = useState("");
+  const [mCount, setMCount] = useState('');
   const [categoryPie, setCategoryPie] = useState({});
   const [orderCount, setOrderCount] = useState({});
   const [stateOrder, setStateOrder] = useState({});
@@ -55,35 +55,35 @@ const CopyDashboard = () => {
   // }
 
   const getCategory = () => {
-    axios.get("/api/totalCategory").then((data) => {
-      console.log("category list is", data.data);
+    axios.get('/api/totalCategory').then((data) => {
+      console.log('category list is', data.data);
       setCategoryList(data.data);
     });
   };
 
   const fetchData = () => {
-    axios.get("/ordersData").then((data) => {
+    axios.get('/ordersData').then((data) => {
       setCount(data.data);
       console.log(data);
     });
   };
 
   const revenueData = () => {
-    axios.get("/revenue").then((da) => {
-      console.log("revenue", da.data);
+    axios.get('/revenue').then((da) => {
+      console.log('revenue', da.data);
       setRevenue(da.data.toFixed(2));
     });
   };
 
   const merchantCount = () => {
-    axios.get("/api/merchant").then((data) => {
-      console.log("merchant count", data.data.length);
+    axios.get('/api/merchant').then((data) => {
+      console.log('merchant count', data.data.length);
       setMCount(data.data.length);
     });
   };
 
   const fetchingRevenueGraph = () => {
-    axios.get("/newTimeGraph").then((response) => {
+    axios.get('/newTimeGraph').then((response) => {
       var data = {
         labels: response.data.date,
         series: [response.data.price],
@@ -92,33 +92,33 @@ const CopyDashboard = () => {
       setGraphPlot(data);
 
       let highValue = response.data.price.reduce((a, b) => a + b, 0);
-      console.log(~~highValue, "highValue");
+      console.log(~~highValue, 'highValue');
 
       //console.log(datasales);
     });
   };
 
   const fetchingRevenuePie = () => {
-    axios.get("/statePie").then((response) => {
+    axios.get('/statePie').then((response) => {
       var data = {
         labels: response.data.state,
         series: response.data.price,
       };
-      console.log("pie chart data", data);
+      console.log('pie chart data', data);
       setPiePlot(data);
       //console.log(datasales);
     });
   };
 
   const fetchingCategoryRevenue = () => {
-    axios.get("/categoryRevenue").then((response) => {
+    axios.get('/categoryRevenue').then((response) => {
       var data = {
         labels: response.data.category,
         series: response.data.revenue,
       };
       setCategoryPie(data);
 
-      console.log("Category wise Pie chart", data);
+      console.log('Category wise Pie chart', data);
     });
   };
 
@@ -153,7 +153,7 @@ const CopyDashboard = () => {
   };
 
   const getOrderCount = () => {
-    axios.get("/orderTime").then((response) => {
+    axios.get('/orderTime').then((response) => {
       let data = {
         labels: response.data.date,
         series: [response.data.orders],
@@ -163,7 +163,7 @@ const CopyDashboard = () => {
   };
 
   const stateCountOrder = () => {
-    axios.get("/stateOrderGraph").then((response) => {
+    axios.get('/stateOrderGraph').then((response) => {
       let data = {
         labels: response.data.state,
         series: [response.data.order],
@@ -173,58 +173,64 @@ const CopyDashboard = () => {
   };
 
   const topProducts = () => {
-    axios.get("/topSelling").then((response) => {
+    axios.get('/topSelling').then((response) => {
       setSellingProducts(response.data);
     });
   };
 
   return (
     <div>
-      <div className="content">
+      <div className='content'>
         <Grid fluid>
           <Row>
             <Col lg={4} sm={6}>
               <StatsCard
-                bigIcon={<i className="pe-7s-server text-warning" />}
-                statsText="Total Orders"
+                bigIcon={<i className='pe-7s-server text-warning' />}
+                statsText='Total Orders'
                 statsValue={count}
-                statsIcon={<i className="fa fa-refresh" />}
-                statsIconText="Updated now"
+                statsIcon={<i className='fa fa-refresh' />}
+                statsIconText='Updated now'
               />
             </Col>
             <Col lg={4} sm={6}>
               <StatsCard
-                bigIcon={<i className="pe-7s-wallet text-success" />}
-                statsText="Revenue"
-                statsValue={"$" + revenue}
-                statsIcon={<i className="fa fa-calendar-o" />}
-                statsIconText="Last day"
+                bigIcon={<i className='pe-7s-wallet text-success' />}
+                statsText='Revenue'
+                statsValue={`$ ${
+                  revenue
+                    ? new Intl.NumberFormat('en-US').format(
+                        parseFloat(revenue).toFixed(2)
+                      )
+                    : ""
+                }`}
+                statsIcon={<i className='fa fa-calendar-o' />}
+                statsIconText='Last day'
               />
             </Col>
 
             <Col lg={4} sm={6}>
               <StatsCard
-                bigIcon={<i className="fa fa-user text-info" />}
-                statsText="Merchants"
+                bigIcon={<i className='fa fa-user text-info' />}
+                statsText='Merchants'
                 statsValue={mCount}
-                statsIcon={<i className="fa fa-refresh" />}
-                statsIconText="Updated now"
+                statsIcon={<i className='fa fa-refresh' />}
+                statsIconText='Updated now'
               />
             </Col>
           </Row>
           <Row>
             <Col md={8}>
               <Card
-                statsIcon="fa fa-history"
-                id="chartHours"
-                title="Total Revenue"
-                category="24 Hours performance"
-                stats="Updated 3 minutes ago"
+                statsIcon='fa fa-history'
+                id='chartHours'
+                title='Total Revenue'
+                category='24 Hours performance'
+                stats='Updated 3 minutes ago'
                 content={
-                  <div className="ct-chart">
+                  <div className='ct-chart'>
                     <ChartistGraph
                       data={graphPlot}
-                      type="Bar"
+                      type='Bar'
                       options={optionsGraphPlot}
                       responsiveOptions={responsiveSales}
                     />
@@ -234,16 +240,16 @@ const CopyDashboard = () => {
             </Col>
             <Col md={4}>
               <Card
-                statsIcon="fa fa-clock-o"
-                title="Revenue"
-                category="State Wise"
-                stats="Campaign sent 2 days ago"
+                statsIcon='fa fa-clock-o'
+                title='Revenue'
+                category='State Wise'
+                stats='Campaign sent 2 days ago'
                 content={
                   <div
-                    id="chartPreferences"
-                    className="ct-chart ct-perfect-fourth"
+                    id='chartPreferences'
+                    className='ct-chart ct-perfect-fourth'
                   >
-                    <ChartistGraph data={piePlot} type="Pie" />
+                    <ChartistGraph data={piePlot} type='Pie' />
                   </div>
                 }
               />
@@ -252,32 +258,32 @@ const CopyDashboard = () => {
           <Row>
             <Col md={4}>
               <Card
-                statsIcon="fa fa-clock-o"
-                title="Revenue by category"
-                category="Category Wise"
-                stats="Campaign sent 2 days ago"
+                statsIcon='fa fa-clock-o'
+                title='Revenue by category'
+                category='Category Wise'
+                stats='Campaign sent 2 days ago'
                 content={
                   <div
-                    id="chartPreferences"
-                    className="ct-chart ct-perfect-fourth"
+                    id='chartPreferences'
+                    className='ct-chart ct-perfect-fourth'
                   >
-                    <ChartistGraph data={categoryPie} type="Pie" />
+                    <ChartistGraph data={categoryPie} type='Pie' />
                   </div>
                 }
               />
             </Col>
             <Col md={8}>
               <Card
-                statsIcon="fa fa-history"
-                id="chartHours"
-                title="Orders"
-                category="Day Wise"
-                stats="Updated 3 minutes ago"
+                statsIcon='fa fa-history'
+                id='chartHours'
+                title='Orders'
+                category='Day Wise'
+                stats='Updated 3 minutes ago'
                 content={
-                  <div className="ct-chart">
+                  <div className='ct-chart'>
                     <ChartistGraph
                       data={orderCount}
-                      type="Line"
+                      type='Line'
                       options={orderCountGraphPlot}
                       responsiveOptions={responsiveSales}
                     />
@@ -290,16 +296,16 @@ const CopyDashboard = () => {
           <Row>
             <Col md={8}>
               <Card
-                statsIcon="fa fa-history"
-                id="chartHours"
-                title="Orders"
-                category="State Wise"
-                stats="Updated 3 minutes ago"
+                statsIcon='fa fa-history'
+                id='chartHours'
+                title='Orders'
+                category='State Wise'
+                stats='Updated 3 minutes ago'
                 content={
-                  <div className="ct-chart">
+                  <div className='ct-chart'>
                     <ChartistGraph
                       data={stateOrder}
-                      type="Bar"
+                      type='Bar'
                       options={stateOrderCountGraphPlot}
                       responsiveOptions={responsiveSales}
                     />
@@ -312,11 +318,11 @@ const CopyDashboard = () => {
           <Row>
             <Col md={8}>
               <Card
-                title="Top Selling Products"
+                title='Top Selling Products'
                 ctTableFullWidth
                 ctTableResponsive
                 content={
-                  <Table striped hover size="sm">
+                  <Table striped hover size='sm'>
                     <thead>
                       <tr>
                         <th>Sku</th>
